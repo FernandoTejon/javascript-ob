949c4f651650ff90cd50639dbca808f66a43b6c2
FernandoTejon <nandotc7@gmail.com>
2023-03-02 12:44:00 +0100

    Añadido el ejercicio del bloque 5 sobre números

---
+++ Ejercicios/ejercicio-bloque-05.js
@@ -0,0 +1,11 @@
+let alt = 180;
+let altm = 1.80;
+let pes = 95.6;
+let altmr = Math.ceil(altm);
+let pesr = Math.floor(pes);
+let numberEqual = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+// Comprobaciones
+console.log(altmr);
+console.log(pesr);
+console.log(numberEqual);
